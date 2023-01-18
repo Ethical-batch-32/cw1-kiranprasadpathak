70e29d7d26f66df5ea6ba21a66b7d2f36f4973e7
kiranprasadpathak <108648687+kiranprasadpathak@users.noreply.github.com>
2023-01-18 18:14:26 +0545

    Add files via upload

---
+++ cars website/cars website/register.php
@@ -0,0 +1,25 @@
+<script src="https://use.fontawesome.com/b60cec5f1c.js"></script>
+
+<h1>Create An Account</h1>
+<div class="box">
+  <input type="text" placeholder="Display name*">
+  <i class="fa fa-user-o" aria-hidden="true"></i>
+</div>
+<div class="box">
+  <input type="email" placeholder="Email*">
+  <i class="fa fa-envelope-o" aria-hidden="true"></i>
+</div>
+<div class="box">
+  <input type="password" placeholder="Password*">
+  <i class="fa fa-key" aria-hidden="true"></i>
+</div>
+<div class="box">
+  <input type="password" placeholder="Confirm password*">
+  <i class="fa fa-key" aria-hidden="true"></i>
+</div>
+<div class="box">
+  <input type="password" placeholder="Register code*">
+  <i class="fa fa-key" aria-hidden="true"></i>
+</div>
+
+<input type="button" onclick="alert('Go Study Please')" value="Register">
